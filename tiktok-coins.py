428ab73d3e60b52dd6da6915b0cedac341046291
mario <145112146+Marioooo32@users.noreply.github.com>
2023-09-20 15:24:51 +0300

    tiktok-coins.py

--- tiktok-coins.py
+++ tiktok-coins.py
@@ -1,6 +1,6 @@
 import os
 
-def generate_fake_coins(username, coins):
+def generate_coins(username, coins):
     return coins
 
 while True:
@@ -8,8 +8,8 @@ while True:
     os.system('color 0e')
     username = input("Enter your TikTok username: ")
     coins = int(input("Enter the desired value of coins: "))
-    fake_coins = generate_fake_coins(username, coins)
-    print("Congratulations, @" + username + "! You have generated", fake_coins, "TikTok coins.")
+    coins = generate_coins(username, coins)
+    print("Congratulations, @" + username + "! You have generated", coins, "TikTok coins.")
     
     repeat = input("Do you want to repeat the process? (yes/no): ")
     if repeat.lower() != "yes":
